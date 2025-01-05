db790b8dec5ccbcada2508592faea25a822939e0
kbnopro <tuankhanhb00@gmail.com>
2025-01-05 15:34:51 +0700

    feat: handle timeout for notification

--- ags/src/features/notification/widgets/Notification/index.tsx
+++ ags/src/features/notification/widgets/Notification/index.tsx
@@ -2,6 +2,7 @@ import configOptions from "@config";
 import { getUrgency, type NotifData } from "@notification";
 import { bind, timeout, Variable } from "astal";
 import { Gtk } from "astal/gtk3";
+import Notifd from "gi://AstalNotifd";
 
 import { Content } from "./Content";
 import { Summary } from "./Summary";
@@ -11,18 +12,29 @@ export class Notification {
   private notifData: NotifData;
   constructor(notifData: NotifData) {
     this.notifData = notifData;
-    const { notifObject } = notifData;
-    const { isPopup } = notifData;
+    const { isPopup, notifObject } = notifData;
     const urgency = getUrgency(notifObject);
     const revealed = Variable(false);
     const revealTimer = timeout(20, () => {
       revealed.set(true);
+    });
+    let notifTimeOut = notifObject.get_expire_timeout();
+    if (notifTimeOut == -1) {
+      // This is the case when timeout is not set by application
+      notifTimeOut
+        = getUrgency(notifData.notifObject) == "critical" ? 8000 : 3000;
+    }
+    // Checking the behaviour, either notify-send or Notifd actually handle the timeout
+    // However, it safe to implement this ourself just in case
+    const timeoutTimer = timeout(notifTimeOut, () => {
+      this.close();
     });
 
     this.widget = (
       <revealer
         onDestroy={() => {
           revealTimer.cancel();
+          timeoutTimer.cancel();
         }}
         revealChild={bind(revealed)}
         transitionType={Gtk.RevealerTransitionType.SLIDE_DOWN}
@@ -31,6 +43,7 @@ export class Notification {
         <box homogeneous={true}>
           <eventbox
             onHoverLost={() => {
+              // On hover lost should behave similar to expired signall
               if (isPopup) this.close();
             }}
           >
@@ -53,7 +66,15 @@ export class Notification {
   }
 
   public getWidget = () => this.widget;
-  public close = () => {
+  public close = (
+    reason: Notifd.ClosedReason = Notifd.ClosedReason.UNDEFINED,
+  ) => {
+    // The non-popup notification should not be closed when notification expired
+    // This expiration is triggered by notify-send
+    if (reason == Notifd.ClosedReason.EXPIRED && !this.notifData.isPopup)
+      return false;
     this.widget.destroy();
+    return true;
+    // Return whether the widget is closed or not
   };
 }
--- ags/src/features/notification/widgets/NotificationPopup.tsx
+++ ags/src/features/notification/widgets/NotificationPopup.tsx
@@ -22,7 +22,10 @@ class NotificationMap {
   public resolve = (id: number) => {
     const notification = this.map.get(id);
     if (!notification) return;
+    // this notification should be closed regardless of reasons
     notification.close();
+    // Without the notification center, some popup will never be deleted from the list
+    // However, thanks to the id cycle implemented, the chance of memory leak is low
     this.map.delete(id);
   };
 
@@ -43,13 +46,9 @@ export const NotificationPopup = () => {
             box.pack_end(notificationMap.notify(id), false, false, 0);
             box.show_all();
           })
-          .hook(
-            notifd,
-            "resolved",
-            (_box, id: number, _reason: Notifd.ClosedReason) => {
-              notificationMap.resolve(id);
-            },
-          )}
+          .hook(notifd, "resolved", (_box, id: number) => {
+            notificationMap.resolve(id);
+          })}
     />
   );
 };
